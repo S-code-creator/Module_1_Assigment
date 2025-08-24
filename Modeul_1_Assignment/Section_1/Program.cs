4208d89eb0ffefdefe97b23cc3a449b2c510bb4f
Md. Sabbir Hossen <mdshabbirhossen062@gmail.com>
2025-08-24 23:30:56 +0600

    Add files via upload

---
+++ Modeul_1_Assignment/Section_1/Program.cs
@@ -0,0 +1,35 @@
+﻿//Task 1
+class Car
+{
+   
+   //step 1
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public int Mileage { get; set; }
+
+    // step 2
+    public void Drive(int kilometers)
+    {
+        Mileage += kilometers;
+        Console.WriteLine($"Car {Brand} {Model}  driven {kilometers} km. Total mileage: {Mileage} km.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Car car1 = new Car
+        {
+            Brand = "BMW",
+            Model = "BMW i7",
+            Year = 2022,
+            Mileage = 625
+        };
+
+        car1.Drive(50);
+        
+    }
+}
---
+++ Modeul_1_Assignment/Section_2/Program.cs
@@ -0,0 +1,45 @@
+﻿//task 2
+class Car
+{
+    
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public int Mileage { get; set; }
+
+    // task 2
+    public void ShowCarInfo()
+    {
+        Console.WriteLine($"Car Info: Brand - {Brand}, Model - {Model}, Year - {Year}, Mileage - {Mileage}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Object 1
+        Car car1 = new Car
+        {
+            Brand = "Toyota",
+            Model = "Corolla",
+            Year = 2020,
+            Mileage = 80
+        };
+
+        // Object 2
+        Car car2 = new Car
+        {
+            Brand = "Tesla",
+            Model = "Model 3",
+            Year = 2022,
+            Mileage = 120
+        };
+
+       
+        car1.ShowCarInfo();
+        car2.ShowCarInfo();
+    }
+}
+
+
